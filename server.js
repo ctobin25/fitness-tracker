9a18f6f455dc8f006a5a6ebe8c03622ff4bd990f
Caroline Tobin <ctobin25>
2021-04-10 14:40:16 -0400

    started server

---
+++ server.js
@@ -0,0 +1,32 @@
+
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+
+const PORT = process.env.PORT || 8080;
+
+
+
+
+const app = express();
+
+
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+
+
+
+app.use("/api", require("./routes/api-routes.js"));
+app.use("/", require("./routes/html-routes.js"));
+
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
