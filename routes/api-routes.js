27110fa67de6fabc0f9ddd54a4ae4e17b2bf2646
Caroline Tobin <ctobin25>
2021-04-11 10:54:00 -0400

    get route for workout

---
+++ routes/api-routes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const db = require("../models");
+
+router.get("/workouts", (req, res) => {
+    db.Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
